Fix softmax dim

sgg/model.py
@@ -228,7 +228,7 @@ class GraphSeq2Seq(nn.Module):
                 #indices = torch.topk(decoder_output, 1).indices
                 #decoder_input = indices.view(1, 1, 3)
 
-                decoder_output = decoder_output.squeeze(0).softmax(dim=1)
+                decoder_output = decoder_output.squeeze(0).softmax(dim=2)
                 # Sample from a multinomial distribution
                 indices = torch.multinomial(decoder_output.squeeze(0), 1)
                 decoder_input = indices.view(1, 1, 3)